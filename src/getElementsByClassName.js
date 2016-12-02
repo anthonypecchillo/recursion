9a7a901c41450db571ce5f821c4debc2dab4fc3b
anthonypecchillo <anthonypecchillo@gmail.com>
2016-12-02 09:36:44 -0800

    Solved getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,84 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+
+
+var getElementsByClassName = function (className) {
+
+  var elementsWithClass = [];
+  var startPosition = document.body;
+
+  var innerFunction = function(newPosition) {
+  	if (newPosition.className.split(' ').indexOf(className) >= 0) {
+  		elementsWithClass.push(newPosition);
+  	}
+    if (newPosition.children.length > 0) {
+  	  for (var i = 0; i < newPosition.children.length; i++) {
+  	  	innerFunction(newPosition.children[i]);
+  	  }
+  	}
+  };
+  innerFunction(startPosition);
+  return elementsWithClass;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var getElementsByClassName = function(className, node){
+  
+//   var nodes = [];
+
+//   node = node || document.body;
+    
+//   var parts = node.className.split(' ');
+
+//   if (parts.indexOf(className) >= 0) {
+//     nodes.push(node);
+//   }
+
+//   for (var i = 0; i < node.children.length; i++) {
+//     var childResults = getElementsByClassName(className, node.children[i]);
+//     nodes = nodes.concat(childResults);
+//   }
+
+//   return nodes;
+
+// };
